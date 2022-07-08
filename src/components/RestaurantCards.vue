579bec3bc6726655a3bbcd00919dd9926db88424
lien <37633033+lienweb@users.noreply.github.com>
2022-07-09 03:37:41 +0800

    finish navPills

---
+++ src/components/RestaurantCards.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="col-md-6 col-lg-4">
+    <div class="card mb-4">
+      <img class="card-img-top" :src="restaurant.image" alt="Card image cap" width="286px" height="180px">
+      <div class="card-body">
+        <p class="card-text title-wrap">
+          <a href="#">
+            {{ restaurant.name }}
+          </a>
+        </p>
+        <span class="badge bg-secondary">{{ restaurant.Category ? restaurant.Category.name : '未分類' }}</span>
+        <p class="card-text text-truncate">
+          {{ restaurant.description }}
+        </p>
+      </div>
+      <div class="card-footer">
+        <button type="button" class="btn btn-danger btn-border favorite mr-2" v-if="restaurant.isFavorited"
+          @click.stop.prevent="deleteFavorite">
+          移除最愛
+        </button>
+        <button type="button" class="btn btn-primary btn-border favorite mr-2" v-else @click.stop.prevent="addFavorite">
+          加到最愛
+        </button>
+        <button type="button" class="btn btn-danger like mr-2" v-if="restaurant.isLiked"
+          @click.stop.prevent="deleteFromLike">
+          Unlike
+        </button>
+        <button type="button" class="btn btn-primary like mr-2" v-else @click.stop.prevent="addToLike">
+          Like
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  props: {
+    initialRestaurant: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      restaurant: this.initialRestaurant
+    }
+  },
+  methods: {
+    addFavorite() {
+      this.restaurant = {
+        ...this.restaurant, // 保留餐廳內原有資料
+        isFavorited: true
+      }
+      // this.restaurant.isFavorited = true
+    },
+    deleteFavorite() {
+      this.restaurant = {
+        ...this.restaurant, // 保留餐廳內原有資料
+        isFavorited: false
+      }
+      // this.restaurant.isFavorited = false
+    },
+    addToLike() {
+      this.restaurant = {
+        ...this.restaurant, // 保留餐廳內原有資料
+        isLiked: true
+      }
+      // this.restaurant.isLiked = true
+    },
+    deleteFromLike() {
+      this.restaurant = {
+        ...this.restaurant, // 保留餐廳內原有資料
+        isLiked: false
+      }
+      // this.restaurant.isLiked = false
+    }
+  }
+}
+</script>
---
+++ src/components/RestaurantsNavPills.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="nav nav-pills mb-4">
+    <li class="nav-item">
+      <router-link class="nav-link" :to="{ name: 'restaurants' }">
+        全部
+      </router-link>
+    </li>
+
+    <li v-for="category in categories" :key="category.id" class="nav-item">
+      <router-link class="nav-link" :to="{ name: 'restaurants', query: { categoryId: category.id } }">
+        <!-- 讓這個連結代query string -->
+        {{ category.name }}
+      </router-link>
+    </li>
+  </ul>
+</template>
+<!-- <template>
+  <ul class="nav nav-pills mb-4">
+    <li class="nav-item">
+      <router-link class="nav-link" data-bs-toggle="pill" data-bs-target="#pills-profile"  type="button" role="tab"
+        aria-controls="pills-profile" aria-selected="false" :to="{ name: 'restaurants' }">全部</router-link>
+    </li>
+    <li class="nav-item" v-for="category in categories" :key="category.id">
+      <router-link class="nav-link" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab"
+        aria-controls="pills-profile" aria-selected="false"
+        :to="{ name: 'restaurants', query: { categoryId: category.id } }">
+        {{ category.name }}
+      </router-link>
+    </li>
+  </ul>
+</template> -->
+
+<script>
+export default {
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
