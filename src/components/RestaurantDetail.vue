8f879f3ce272616240c8cadff71d3cd00a2e4cba
lien <37633033+lienweb@users.noreply.github.com>
2022-07-10 01:51:36 +0800

    restaurant detail page

---
+++ src/components/RestaurantDetail.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="row">
+    <div class="col-md-12 mb-3">
+      <h1>{{ restaurantDetail.name }}</h1>
+      <p class="badge bg-secondary mt-1 mb-3">
+        {{ restaurantDetail.categoryName }}
+      </p>
+    </div>
+    <div class="col-lg-4">
+      <img class="img-fluid d-block" :src="restaurantDetail.image" style="width: 250px;margin-bottom: 25px;">
+      <div class="contact-info-wrap">
+        <ul class="list-unstyled">
+          <li>
+            <strong>Opening Hour:</strong>
+            {{ restaurantDetail.openingHours }}
+          </li>
+          <li>
+            <strong>Tel:</strong>
+            {{ restaurantDetail.tel }}
+          </li>
+          <li>
+            <strong>Address:</strong>
+            {{ restaurantDetail.address }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-lg-8">
+      <p>{{ restaurantDetail.description }}</p>
+      <router-link class="btn btn-primary btn-border me-2" :to="{ name: 'restaurants' }">Dashboard</router-link>
+
+      <button type="button" class="btn btn-danger btn-border me-2" v-if="restaurantDetail.isFavorited"
+        @click.stop.prevent="deleteFavorite">
+        移除最愛
+      </button>
+      <button type="button" class="btn btn-primary btn-border me-2" v-else @click.stop.prevent="addFavorite">
+        加到最愛
+      </button>
+      <button type="button" class="btn btn-danger like me-2" v-if="restaurantDetail.isLiked"
+        @click.stop.prevent="deleteLike">
+        Unlike
+      </button>
+      <button type="button" class="btn btn-primary like me-2" v-else @click.stop.prevent="addLike">
+        Like
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialRestaurantDetail: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      restaurantDetail: this.initialRestaurantDetail
+    }
+  },
+  methods: {
+    addFavorite () {
+      this.restaurantDetail = {
+        ...this.restaurantDetail,
+        isFavorited: true
+      }
+    },
+    deleteFavorite () {
+      this.restaurantDetail = {
+        ...this.restaurantDetail,
+        isFavorited: false
+      }
+    },
+    addLike () {
+      this.restaurantDetail = {
+        ...this.restaurantDetail,
+        isLiked: true
+      }
+    },
+    deleteLike () {
+      this.restaurantDetail = {
+        ...this.restaurantDetail,
+        isLiked: false
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/Restaurant.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="container py-5">
+    <!-- 餐廳資訊頁 RestaurantDetail -->
+    <RestaurantDetail :initial-restaurant-detail="restaurant"/>
+    <!-- 餐廳評論 RestaurantComments -->
+    <!-- 新增評論 CreateComment -->
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import RestaurantDetail from '../components/RestaurantDetail.vue'
+
+const dummyData = {
+  "restaurant": {
+    "id": 1,
+    "name": "Remington Shields",
+    "tel": "(664) 474-5747 x14320",
+    "address": "9340 Cornell Field",
+    "opening_hours": "08:00",
+    "description": "Et beatae molestiae optio quaerat dolores autem.\nDolorem et iste.",
+    "image": "https://loremflickr.com/320/240/restaurant,food/?random=14.732919933876175",
+    "viewCounts": 1,
+    "createdAt": "2022-07-07T09:45:55.000Z",
+    "updatedAt": "2022-07-09T15:27:59.096Z",
+    "CategoryId": 2,
+    "Category": {
+      "id": 2,
+      "name": "日本料理",
+      "createdAt": "2022-07-07T09:45:55.000Z",
+      "updatedAt": "2022-07-07T09:45:55.000Z"
+    },
+    "FavoritedUsers": [],
+    "LikedUsers": [],
+    "Comments": [
+      {
+        "id": 101,
+        "text": "Quasi nesciunt quasi rerum et fugit deleniti et laboriosam eligendi.",
+        "UserId": 3,
+        "RestaurantId": 1,
+        "createdAt": "2022-07-07T09:45:55.000Z",
+        "updatedAt": "2022-07-07T09:45:55.000Z",
+        "User": {
+          "id": 3,
+          "name": "user2",
+          "email": "user2@example.com",
+          "password": "$2a$10$AO.O77pnpFTWGbvBvEK/OOBaAxA93X9AlBk7NZOYyck0IRcooLudm",
+          "isAdmin": false,
+          "image": null,
+          "createdAt": "2022-07-07T09:45:55.000Z",
+          "updatedAt": "2022-07-07T09:45:55.000Z"
+        }
+      },
+      {
+        "id": 51,
+        "text": "Vel dolores veniam voluptas adipisci nihil placeat voluptates perspiciatis rerum.",
+        "UserId": 2,
+        "RestaurantId": 1,
+        "createdAt": "2022-07-07T09:45:55.000Z",
+        "updatedAt": "2022-07-07T09:45:55.000Z",
+        "User": {
+          "id": 2,
+          "name": "user1",
+          "email": "user1@example.com",
+          "password": "$2a$10$SJp6xLGGUfwp4xZyow1x9eZMPicZcccBu7SkC5syFab6J/1HQ7UXS",
+          "isAdmin": false,
+          "image": null,
+          "createdAt": "2022-07-07T09:45:55.000Z",
+          "updatedAt": "2022-07-07T09:45:55.000Z"
+        }
+      },
+      {
+        "id": 1,
+        "text": "Architecto molestiae illum.",
+        "UserId": 2,
+        "RestaurantId": 1,
+        "createdAt": "2022-07-07T09:45:55.000Z",
+        "updatedAt": "2022-07-07T09:45:55.000Z",
+        "User": {
+          "id": 2,
+          "name": "user1",
+          "email": "user1@example.com",
+          "password": "$2a$10$SJp6xLGGUfwp4xZyow1x9eZMPicZcccBu7SkC5syFab6J/1HQ7UXS",
+          "isAdmin": false,
+          "image": null,
+          "createdAt": "2022-07-07T09:45:55.000Z",
+          "updatedAt": "2022-07-07T09:45:55.000Z"
+        }
+      }
+    ]
+  },
+  "isFavorited": false,
+  "isLiked": false
+}
+
+export default {
+  name: 'Restaurant',
+  components: { // 註冊才能用
+    RestaurantDetail
+  },
+  data() {
+    return {
+      restaurant: {
+        id: -1,
+        name: '',
+        categoryName: '',
+        image: '',
+        openingHours: '',
+        tel: '',
+        address: '',
+        description: '',
+        isFavorited: false,
+        isLiked: false
+      },
+      restaurantComments: [],
+      // id: this.$route.params.id
+    }
+  },
+  methods: {
+    // fetchRestaurant(id) {
+    //   this.restaurant = dummyData.restaurant
+    // }
+    fetchRestaurant(restaurantId) {
+      console.log('fetchRestaurant id: ', restaurantId)
+
+      // 解構dummyData
+      const { restaurant, isFavorited, isLiked } = dummyData
+      const {
+        id, name, Category, image, opening_hours, tel, address, description
+      } = restaurant
+
+      this.restaurant = {
+        id,
+        name,
+        categoryName: Category.name ? Category.name : '未分類',
+        image,
+        openingHours: opening_hours,
+        tel,
+        address,
+        description,
+        isFavorited,
+        isLiked
+      }
+
+      // this.restaurant = {
+      //   id: dummyData.restaurant.id,
+      //   name: dummyData.restaurant.name,
+      //   categoryName: dummyData.restaurant.Category.name,
+      //   image: dummyData.restaurant.image,
+      //   openingHours: dummyData.restaurant.opening_hours,
+      //   tel: dummyData.restaurant.tel,
+      //   address: dummyData.restaurant.address,
+      //   description: dummyData.restaurant.description,
+      //   isFavorited: dummyData.isFavorited,
+      //   isLiked: dummyData.isLiked,
+      // }
+
+      this.restaurantComments = dummyData.restaurant.Comments
+    }
+  },
+  created() {
+    const { id: restaurantId } = this.$route.params
+    this.fetchRestaurant(restaurantId)
+  }
+}
+</script>
